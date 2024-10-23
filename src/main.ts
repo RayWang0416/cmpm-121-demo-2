58870532c54b15dd2f4e0c0c2a3d4a51ddc0b3ff
RayWang0416 <2328324146@qq.com>
2024-10-23 10:35:31 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,8 @@ app.innerHTML = `<h1>${APP_NAME}</h1>`;
 
 //canvas constants and variables
 let isDrawing = false;
-let x = 0;
-let y = 0;
 let lines: Array<Array<{ x: number; y: number }>> = [];
+let redoStack: Array<Array<{ x: number; y: number }>> = [];
 
 //Canvas
 const canvas = document.createElement("canvas");
@@ -19,14 +18,26 @@ canvas.width = 256;
 canvas.height = 256;
 canvas.id = "canvas1";
 app.appendChild(canvas);
+app.appendChild(document.createElement("br"));
 
-//canvas drawing
+//CANVAS DRAWING
+//drawing changed event
+canvas.addEventListener("drawing-changed", () => {
+    if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redrawCanvas(ctx, lines);
+    }
+});
+
+//mouse down
 canvas.addEventListener("mousedown", (e) => {
     isDrawing = true;
+    redoStack = [];
     lines.push([{ x: e.offsetX, y: e.offsetY }]);
     canvas.dispatchEvent(new Event("drawing-changed"));
 });
-  
+
+//mouse move
 canvas.addEventListener("mousemove", (e) => {
     if (isDrawing) {
         const currentSegment = lines[lines.length - 1];
@@ -34,16 +45,10 @@ canvas.addEventListener("mousemove", (e) => {
         canvas.dispatchEvent(new Event("drawing-changed"));
     }
 });
-  
+
+//mouse up
 window.addEventListener("mouseup", () => {
     isDrawing = false;
-});
-  
-canvas.addEventListener("drawing-changed", () => {
-    if (ctx) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        redrawCanvas(ctx, lines);
-    }
 });
 
 function redrawCanvas(context: CanvasRenderingContext2D, line: Array<Array<{ x: number; y: number }>>) {
@@ -69,8 +74,39 @@ app.appendChild(clearButton);
 
 clearButton.addEventListener("click", () => {
     lines = [];
+    redoStack = [];
     if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+//undo button
+const undoButton = document.createElement("button");
+undoButton.innerText = "Undo";
+app.appendChild(undoButton);
+
+undoButton.addEventListener("click", () => {
+    if (lines.length > 0) {
+        const lastLine = lines.pop();
+        if (lastLine) {
+            redoStack.push(lastLine);
+            canvas.dispatchEvent(new Event("drawing-changed"));
+        }
+    }
+});
+
+//redo button
+const redoButton = document.createElement("button");
+redoButton.innerText = "Redo";
+app.appendChild(redoButton);
+
+redoButton.addEventListener("click", () => {
+    if (redoStack.length > 0) {
+        const lineToRedo = redoStack.pop();
+        if (lineToRedo) {
+            lines.push(lineToRedo);
+            canvas.dispatchEvent(new Event("drawing-changed"));
+        }
     }
 });
   
