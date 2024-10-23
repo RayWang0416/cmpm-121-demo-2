25211de4707e15436a5ee468801fac7692b01e52
RayWang0416 <2328324146@qq.com>
2024-10-23 09:52:53 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,63 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 app.innerHTML = `<h1>${APP_NAME}</h1>`;
 
+//canvas constants and variables
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
 //Canvas
 const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 256;
 canvas.height = 256;
 canvas.id = "canvas1";
 app.appendChild(canvas);
+
+//canvas drawing
+canvas.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing && ctx) {
+        drawLine(ctx, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+  
+window.addEventListener("mouseup", (e) => {
+    if (isDrawing && ctx) {
+        drawLine(ctx, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+});
+
+function drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+
+//clear canvas button
+const clearButton = document.createElement("button");
+clearButton.innerText = "Clear";
+app.appendChild(clearButton);
+
+clearButton.addEventListener("click", () => {
+    if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+  
+
