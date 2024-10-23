85f1afef03861f3ac9250f42a5ede5dffa45d5a6
RayWang0416 <2328324146@qq.com>
2024-10-23 07:11:01 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Cat";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = `<h1>${APP_NAME}</h1>`;
+
+//Canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "canvas1";
+app.appendChild(canvas);
